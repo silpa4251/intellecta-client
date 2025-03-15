c24e183c3e29d44982f9e26546b7d490d7f5a712
fariskt <fariskt00@gmail.com>
2025-03-15 15:19:00 +0530

    updated course detials

--- src/pages/Courses/CourseDetails.tsx
+++ src/pages/Courses/CourseDetails.tsx
@@ -41,8 +41,11 @@ const CourseDetails = () => {
               </div>
             </div>
           </div>
-
-          <div></div>
+          <div>
+            <div>
+                <h3></h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
