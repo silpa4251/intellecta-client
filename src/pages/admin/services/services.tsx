df9e39b77531a3a7c1877b732c38bfe500d24f05
shadilvp <shadilx8@gmail.com>
2025-04-22 15:49:04 +0530

    completed notification setup

--- src/pages/admin/services/services.tsx
+++ src/pages/admin/services/services.tsx
@@ -34,7 +34,14 @@ interface NotificationPayload {
 }
 
 export const sendNotification = async (notificationData: NotificationPayload) => {
+  console.log("notificaiton data from service : ", notificationData)
   const response = await axiosInstance.post("http://localhost:5008/api/notification/send", notificationData);
   console.log(response.data)
   return response.data
 }
+
+export const getCurentAdmin = async () => {
+  const response = await axiosInstance.get("http://localhost:5000/api/user/getuserbyid");
+  console.log(response.data)
+  return response.data
+} 
