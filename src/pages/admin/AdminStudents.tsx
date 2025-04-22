086ebd24a3f265a829547697c53317cb4aa4211f
fariskt <fariskt00@gmail.com>
2025-04-22 20:36:50 +0530

    added top performers

--- src/pages/admin/AdminStudents.tsx
+++ src/pages/admin/AdminStudents.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from "react";
 import DeleteButton from "../../utils/ui/deleteButton";
 import { fetchStudents } from "./services/services";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import AllStudentsLoader from "../../utils/ui/allStudentsLoader";
-
-
-
 
 export interface Student {
   _id: string;
@@ -20,38 +16,14 @@ export interface Student {
 }
 
 const AdminStudents = () => {
-
-  const {
-    data: students,
-    isLoading,
-    isError,
-    error,
-  } = useQuery<Student[]>({
+  const { data: students, isPending } = useQuery({
     queryKey: ["students"],
     queryFn: fetchStudents,
   });
 
-  console.log(students);
-
   const handleDelete = (id: number) => {
     console.log("Delete student with ID:", id);
   };
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center h-[75vh]">
-        <AllStudentsLoader/>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className="p-6 text-center text-red-600">
-        Failed to load students. {String(error)}
-      </div>
-    );
-  }
 
   return (
     <div className="p-6">
@@ -70,52 +42,57 @@ const AdminStudents = () => {
               </tr>
             </thead>
             <tbody>
-              {students && students?.map((student, index) => (
-                <tr
-                  key={student._id + index}
-                  className={`transition-colors duration-200 ${
-                    index % 2 === 0
-                      ? "bg-[#BBD2CB] text-[#4F4F4F] "
-                      : "bg-[#ffffff] text-[#4F4F4F] "
-                  }`}
-                >
-                  <td className="p-4">
-                    <img
-                      src={
-                        student.profilePic
-                      }
-                      alt={student.name}
-                      className="w-10 h-10 rounded-full object-cover"
-                    />
-                  </td>
-                  <td className="p-4">{student.name}</td>
-                  <td className="p-4">{student.email}</td>
-                  <td className="p-4">{student.age}</td>
-                  <td className="p-4">{student.phone}</td>
-                  <td className="p-4">
-                    <span
-                    //   className={`px-2 py-1 rounded-full text-xs font-medium 
-                    //     ${
-                    //     student.status === "Active"
-                    //       ? "bg-green-100 text-green-600"
-                    //       : "bg-red-100 text-red-600"
-                    //   }
-                    //   `
-                    // }
-                    >
-                      active
-                    </span>
-                  </td>
-                  <td className="p-4 text-center">
-                    <button
-                      // onClick={() => handleDelete(student._id)}
-                      className="text-red-500 hover:text-red-700 transition font-medium"
-                    >
-                      <DeleteButton />
-                    </button>
-                  </td>
-                </tr>
-              ))}
+              {isPending ? (
+                <div className="flex justify-center items-center h-[75vh] w-screen">
+                  <AllStudentsLoader />
+                </div>
+              ) : (
+                students?.length > 0 &&
+                students?.map((student: Student, index: number) => (
+                  <tr
+                    key={student._id + index}
+                    className={`transition-colors duration-200 ${
+                      index % 2 === 0
+                        ? "bg-[#BBD2CB] text-[#4F4F4F] "
+                        : "bg-[#ffffff] text-[#4F4F4F] "
+                    }`}
+                  >
+                    <td className="p-4">
+                      <img
+                        src={student.profilePic}
+                        alt={student.name}
+                        className="w-10 h-10 rounded-full object-cover"
+                      />
+                    </td>
+                    <td className="p-4">{student.name}</td>
+                    <td className="p-4">{student.email}</td>
+                    <td className="p-4">{student.age}</td>
+                    <td className="p-4">{student.phone}</td>
+                    <td className="p-4">
+                      <span
+                      //   className={`px-2 py-1 rounded-full text-xs font-medium
+                      //     ${
+                      //     student.status === "Active"
+                      //       ? "bg-green-100 text-green-600"
+                      //       : "bg-red-100 text-red-600"
+                      //   }
+                      //   `
+                      // }
+                      >
+                        active
+                      </span>
+                    </td>
+                    <td className="p-4 text-center">
+                      <button
+                        // onClick={() => handleDelete(student._id)}
+                        className="text-red-500 hover:text-red-700 transition font-medium"
+                      >
+                        <DeleteButton />
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
